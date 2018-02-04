54cefb592595f5dd727ed4882fee987ad0550694
Chris Hawkins <catchcoder@gmail.com>
2018-02-04 12:03:40 +0000

    Initial commit

--- route.py
+++ route.py
@@ -2,12 +2,51 @@
 My first flask web site
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template , request, redirect
+import time
+import os
+import sqlite3
+
 
 app = Flask('__name__')
+db_location ='data.sqlite3'
+db_table = 'visits'
+conn = sqlite3.connect(db_location)
+cur = conn.cursor()
+
+sql = ' create table if not exists {} (id integer)'.format(db_table)
+cur.execute(sql)
+conn.commit()
+
+# try:
+    # Try and import GPIO for Raspberry Pi,
+    # If it fails import fake GPIO for CI.
+#     import RPi.GPIO as GPIO
+# except ImportError:
+    # Import fake GPIO https://pypi.python.org/pypi/fakeRPiGPIO/0.2a0.
+from RPi import GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO.setup(17,GPIO.OUT)
+GPIO.setup(2, GPIO.OUT)
+GPIO.output(2, False)
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+
+@app.route('/ledon')
+def ledon():
+    GPIO.output(17,True)
+    return render_template('index.html')
+
+
+@app.route('/ledoff')
+def ledoff():
+    GPIO.output(17,False)
     return render_template('index.html')
 
 if __name__ == '__main__':
